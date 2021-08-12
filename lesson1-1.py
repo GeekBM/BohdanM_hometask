b11470d6cdb0fce042429823b5832702b15ec68a
Bohdan <medvedev.bogdan@gmail.com>
2021-08-12 14:15:38 +0300

    Initial commit

---
+++ lesson1-1.py
@@ -0,0 +1,8 @@
+a = 3
+b = 5
+print('a-', a, 'b-', b)
+string1 = input('Enter first string')
+number1 = int(input('Enter first number'))
+string2 = input('Enter second string')
+number2 = int(input('Enter second number'))
+print(string1, number1, string2, number2)
