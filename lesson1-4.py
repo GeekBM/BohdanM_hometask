63594071ea68a20dbe3a47d7dc89b46bb2d7f08e
GeekBM <88398238+GeekBM@users.noreply.github.com>
2021-08-16 16:43:03 +0300

    Delete lesson1-4.py

--- lesson1-4.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter a positive integer "))
-max = 0
-while n > 0:
-    if n % 10 > max:
-        max = n % 10
-        if max == 9:
-            break
-    n = n // 10
-print(f'Max digit in number {max}')
