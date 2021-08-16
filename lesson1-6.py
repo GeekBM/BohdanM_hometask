4988e8b97c3ac532b9c5c000fd5ef2dfc406f628
GeekBM <88398238+GeekBM@users.noreply.github.com>
2021-08-16 16:43:18 +0300

    Delete lesson1-6.py

--- lesson1-6.py
+++
@@ -1,7 +0,0 @@
-distance = int(input('Дистанция в первый день - '))
-goal = int(input('Цель - '))
-day = 1
-while distance < goal:
-    day += 1
-    distance *= 1.1
-print(f'Необходимое количество дней {day}')
