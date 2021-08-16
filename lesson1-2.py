e17b87e9dfdeda0189389cce09353ec811185bba
GeekBM <88398238+GeekBM@users.noreply.github.com>
2021-08-16 16:42:51 +0300

    Delete lesson1-2.py

--- lesson1-2.py
+++
@@ -1,6 +0,0 @@
-time = int(input('Enter time in seconds'))
-days = time // (3600 * 24)
-hours = (time - days * 3600 * 24) // 3600
-minutes = (time - (days * 3600 * 24 + hours * 3600)) // 60
-seconds = time - (days * 3600 * 24 + hours * 3600 + minutes * 60)
-print(f'Time in format dd:hh:mm:ss {days:02} : {hours:02} : {minutes:02} : {seconds:02}')
